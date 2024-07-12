edf9b5c598b5bc1b8cf4e14715892796ea4405d1
juanfsouza <juanfsouza@gmail.com>
2024-07-12 01:47:50 -0300

    day 8 back-end integracao com front

--- Day 7/login-page-feat-initial-config/src/app/app.routes.ts
+++ Day 7/login-page-feat-initial-config/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/signup/signup.component';
+import { AuthGuard } from './services/auth-guard.service';
+import { UserComponent } from './pages/user/user.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +12,10 @@ export const routes: Routes = [
     {
         path: "signup",
         component: SignUpComponent
+    },
+    {
+        path: "user",
+        component: UserComponent,
+        canActivate: [AuthGuard],
     }
 ];
---
+++ Day 8/login-auth-api/src/main/java/com/example/login_auth_api/domain/user/User.java
@@ -0,0 +1,43 @@
+package com.example.login_auth_api.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPassword(String encode) {
+    }
+
+    public void setEmail(String email) {
+    }
+
+    public void setName(String name) {
+    }
+}
